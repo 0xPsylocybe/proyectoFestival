f9dc22c56e463bd005b02fc1acb75e2619764bf1
tluizay <luizayvdmx@gmail.com>
2026-08-26 17:45:33 +0200

    actuializaciones

--- artistas/admin.py
+++ artistas/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from models import Generos, Artistas, Origen
 
-@admin.register(Origen)
-class OrigenAdmin(admin.ModelAdmin):
-    search_fields = ("nombre",)
+
     
 @admin.register(Generos)
 class GenerosAdmin(admin.ModelAdmin):
@@ -12,4 +10,4 @@ class GenerosAdmin(admin.ModelAdmin):
 @admin.register(Artistas)
 class ArtistasAdmin(admin.ModelAdmin):
     list_display=("nombre","imagen","descripcion","origen")
-    search_fields =("genero")
+    search_fields =("genero") 
--- artistas/models.py
+++ artistas/models.py
@@ -13,16 +13,25 @@ class Generos(models.Model):
 class Origen(models.Model):
     nombre = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name="Origen"
+        verbose_name_plural="Origen"
+        
+    
     def __str__(self):
         return self.nombre
 
-class Artistas(models.Model):
-    nombre = models.CharField(max_length=100)
-    imagen = models.ImageField(upload_to='artistas/')
-    descripcion = models.TextField()
-    genero = models.ForeignKey(Generos, on_delete=models.CASCADE)
-    origen = models.ForeignKey(Origen, on_delete=models.SET_NULL, null=True) # Relación para el desplegable
 
+class Artistas(models.Model):
+    nombre = models.CharField("nombre",max_length=100)
+    imagen = models.ImageField("imagen",upload_to='artistas/pfp')
+    descripcion = models.CharField("descripcion", max_length=150)
+    genero = models.ForeignKey(Generos, on_delete=models.CASCADE,related_name="genero")
+    origen = models.ForeignKey(Origen, on_delete=models.SET_NULL, null=True, related_name="origen") 
+    class Meta:
+        verbose_name="Artista"
+        verbose_name_plural="Artistas"
+    
     def __str__(self):
         return f"{self.nombre} de {self.origen}"
     
