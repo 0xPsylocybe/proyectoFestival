8cfdc472ab60c32145c0616f612a80c6c59b201e
tluizay <luizayvdmx@gmail.com>
2026-08-26 17:29:30 +0200

    models de artistas

--- artistas/models.py
+++ artistas/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Generos(models.Model):
+    nombre=models.CharField("genero", max_length=20)
+    class Meta:
+        verbose_name="Género"
+        verbose_name_plural="Géneros"
+    
+    def _str_(self):
+        return self.nombre
+
+class Artistas(models.Model):
+    nombre=models.CharField("nombre", max_length=30,null=False,blank=False)
+    imagen=models.ImageField("imagen",upload_to="artistas/pfp", null=True, blank=True)
+    descripcion=models.CharField("descripcion",max_length=200,null=True,blank=True)
+    origen=models.CharField("origen",max_length=20)
+    genero=models.ForeignKey(Generos,  on_delete=models.CASCADE, related_name="genero")
+    
+    class Meta:
+        verbose_name="Artista"
+        verbose_name_plural="Artistas"
+    
+    def __str__(self):
+        return f"{self.nombre} de {self.origen}"
